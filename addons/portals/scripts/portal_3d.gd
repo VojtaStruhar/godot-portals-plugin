6562e843fce9c5970e9c7ce141d34f485f719bdd
Vojtěch Struhár <vojtechstruhar@gmail.com>
2025-03-05 21:20:54 +0100

    Move viewport setup into gameplay space
    - Viewports and cameras are added in _ready
    - Environment tonemapping is reset on portal cameras

--- addons/portals/scripts/portal_3d.gd
+++ addons/portals/scripts/portal_3d.gd
@@ -13,8 +13,7 @@ class_name Portal3D extends Node3D
 ## The width of the frame portal. Adjusts the near clip distance of the camera looking through THIS portal.
 @export var portal_frame_width: float = 0.4
 
-@export var exit_portal: Portal3D:
-	set = _on_exit_portal_set
+@export var exit_portal: Portal3D
 
 # TODO: Make this optional, only display when the other's portal exit_portal is null.
 @export_tool_button("Pair Portals?", "MeshInstance2D")
@@ -43,6 +42,7 @@ const PORTAL_SHADER = preload("uid://bhdb2skdxehes")
 
 ## _ready(), but only in editor.
 func _editor_ready() -> void:
+	print(name + ": _editor_ready")
 	if portal_mesh == null:
 		portal_mesh = MeshInstance3D.new()
 		portal_mesh.name = "PortalMeshInstance"
@@ -56,41 +56,6 @@ func _editor_ready() -> void:
 		
 		self.lock_node(portal_mesh)
 
-
-func _on_exit_portal_set(new_exit_portal: Portal3D) -> void:
-	if not Engine.is_editor_hint(): 
-		print("Game ON: ignoring _on_exit_portal_set")
-		return
-	
-	if portal_camera:
-		print_debug("Freeing existing portal camera")
-		portal_camera.queue_free()
-		portal_camera = null
-	if portal_viewport:
-		print_debug("Freeing existing subviewport")
-		portal_viewport.queue_free()
-		portal_viewport = null
-	
-	exit_portal = new_exit_portal
-	
-	if exit_portal != null:
-		portal_viewport = SubViewport.new()
-		portal_viewport.name = self.name + "_SubViewport"
-		portal_viewport.size = Vector2(
-			ProjectSettings.get_setting("display/window/size/viewport_width"),
-			ProjectSettings.get_setting("display/window/size/viewport_height")
-		)
-		exit_portal.add_child(portal_viewport, true)
-		portal_viewport.owner = self.owner
-		
-		portal_camera = Camera3D.new()
-		portal_camera.name = self.name + "_Camera3D"
-		portal_viewport.add_child(portal_camera, true)
-		portal_camera.owner = self.owner
-		portal_camera.global_position = exit_portal.global_position
-		
-		
-		
 
 func _on_portal_size_changed(new_size: Vector2) -> void:
 	portal_size = new_size
@@ -113,6 +78,8 @@ func _ready() -> void:
 		# It's probably best to assign this manually.
 		player_camera = get_viewport().get_camera_3d()
 	
+	_setup_cameras()
+	
 	var mat: ShaderMaterial = ShaderMaterial.new()
 	print("Portal shader: ", PORTAL_SHADER)
 	mat.shader = PORTAL_SHADER
@@ -130,10 +97,10 @@ func _process(delta: float) -> void:
 		var relative_to_target = exit_portal.portal_mesh.global_transform * flipped
 		
 		portal_camera.global_transform = relative_to_target
-		portal_camera.near = _get_near_plane()
+		portal_camera.near = _calculate_near_plane()
 
 
-func _get_near_plane() -> float:
+func _calculate_near_plane() -> float:
 	var _mesh_aabb: AABB = exit_portal.portal_mesh.get_aabb()
 	
 	var corner_1:Vector3 = exit_portal.to_global(Vector3(_mesh_aabb.position.x, _mesh_aabb.position.y, 0))
@@ -151,6 +118,46 @@ func _get_near_plane() -> float:
 	
 	# The near clip distance is the shortest distance which still contains all the corners
 	return max(0.01, min(d_1, d_2, d_3, d_4) - exit_portal.portal_frame_width)
+
+
+func _setup_cameras() -> void:
+	if Engine.is_editor_hint(): 
+		push_error(name + "._setup_cameras: This should never run in editor!")
+		return
+	
+	if portal_camera:
+		print_debug("[%s] Freeing existing portal camera" % name)
+		portal_camera.queue_free()
+		portal_camera = null
+	if portal_viewport:
+		print_debug("[%s] Freeing existing subviewport" % name)
+		portal_viewport.queue_free()
+		portal_viewport = null
+	
+	if exit_portal != null:
+		portal_viewport = SubViewport.new()
+		portal_viewport.name = self.name + "_SubViewport"
+		portal_viewport.size = Vector2(
+			ProjectSettings.get_setting("display/window/size/viewport_width"),
+			ProjectSettings.get_setting("display/window/size/viewport_height")
+		)
+		exit_portal.add_child(portal_viewport, true)
+		portal_viewport.owner = self.owner
+		
+		# Disable tonemapping on portal cameras
+		var adjusted_env: Environment = player_camera.environment.duplicate() \
+			if player_camera.environment \
+			else player_camera.get_world_3d().environment.duplicate()
+		adjusted_env.tonemap_mode = Environment.TONE_MAPPER_LINEAR
+		adjusted_env.tonemap_exposure = 1
+		
+		portal_camera = Camera3D.new()
+		portal_camera.name = self.name + "_Camera3D"
+		portal_camera.environment = adjusted_env
+		portal_viewport.add_child(portal_camera, true)
+		portal_camera.owner = self.owner
+		portal_camera.global_position = exit_portal.global_position
+		
 
 # ------------------- UTILS ---------------------------
 
