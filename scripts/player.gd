f53a11474c175ffcde73fc33d2d8577711e887fd
Vojtěch Struhár <vojtechstruhar@gmail.com>
2025-03-11 18:26:25 +0100

    Player controller uses velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,34 +18,36 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-		
+	
 	var right:Vector3 = (global_transform.basis.x * Vector3(1, 0, 1)).normalized()
-	var forward:Vector3 = (global_transform.basis.z * Vector3(1, 0, 1)).normalized()
+	var forward:Vector3 = (-global_transform.basis.z * Vector3(1, 0, 1)).normalized()
 	var has_input = false
+
+	velocity.x = 0
+	velocity.z = 0
 
 	if Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A):
 		has_input = true
-		global_translate(-right * SPEED * delta)
+		velocity -= right
 	if Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
 		has_input = true
-		global_translate(right * SPEED * delta)
+		velocity += right
 	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
 		has_input = true
-		global_translate(-forward * SPEED * delta)
+		velocity += forward
 	if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
 		has_input = true
-		global_translate(forward * SPEED * delta)
+		velocity -= forward
 	
-	if has_input == false:
-		velocity.x = 0
-		velocity.z = 0
+	if has_input:
+		var normalized_horizontal_velocity = Vector2(velocity.x, velocity.z).normalized()
+		velocity.x = normalized_horizontal_velocity.x * SPEED
+		velocity.z = normalized_horizontal_velocity.y * SPEED
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	
 	move_and_slide()
