ba8e574ac5a72ab55c47b04480319e7029b3439a
Vojtěch Struhár <vojtechstruhar@gmail.com>
2025-03-17 10:58:58 +0100

    Level buttons in main menu

--- main.gd
+++ main.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@export_dir var levels_directory: String 
+@onready var v_box_container: VBoxContainer = $VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if levels_directory:
+		var dir = DirAccess.open(levels_directory)
+		for file in dir.get_files():
+			if not file.ends_with(".tscn"): continue
+			
+			var b = Button.new()
+			b.text = file.replace(".tscn", "").capitalize()
+			b.pressed.connect(func(): get_tree().change_scene_to_file(levels_directory + "/" + file))
+			v_box_container.add_child(b)
+			
+	
