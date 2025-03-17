32d7a3b62f478c438df2df2842f8fc9195da9bec
Vojtěch Struhár <vojtechstruhar@gmail.com>
2025-03-17 15:17:05 +0100

    Try out modifying project settings

--- addons/portals/plugin.gd
+++ addons/portals/plugin.gd
@@ -1,10 +1,20 @@
 @tool
 extends EditorPlugin
 
+const project_settings_prefix: String = "addons/portals/"
+
+const settings: Dictionary = {
+	"default_teleport_layer": 1 << 4
+}
 
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
-	pass
+	for key in settings.keys():
+		var qual_key = project_settings_prefix + key
+		if not ProjectSettings.has_setting(qual_key):
+			print("Creating project setting: ", key)
+			ProjectSettings.set_setting(qual_key, settings[key])
+			ProjectSettings.set_initial_value(qual_key, settings[key])
+	
 
 
 func _exit_tree() -> void:
