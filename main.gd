1711663bd5f10d3e9e10ebe084ec4b52cebb8fd1
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2025-09-29 17:22:16 +0200

    fixed: load levels through packed scenes instead of looking into directory (fixes export)

--- main.gd
+++ main.gd
@@ -1,24 +1,20 @@
 extends Control
 
-@export_dir var levels_directory: String 
+@export var levels_scenes: Array[PackedScene] = []
 @onready var levels_container: VBoxContainer = %LevelsContainer
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MouseMode.MOUSE_MODE_VISIBLE
 	
-	if levels_directory:
-		var dir = DirAccess.open(levels_directory)
-		for file in dir.get_files():
-			if not file.ends_with(".tscn"): continue
-			
-			var b = Button.new()
-			b.text = file.replace(".tscn", "").capitalize()
-			b.pressed.connect(func(): 
-				print("[Level] " + b.text)
-				get_tree().change_scene_to_file(levels_directory + "/" + file)
-			)
-			levels_container.add_child(b)
+	for level_scene in levels_scenes:
+		var b = Button.new()
+		b.text = level_scene.resource_path.get_file().replace(".tscn", "").capitalize()
+		b.pressed.connect(func(): 
+			print("[Level] " + b.text)
+			get_tree().change_scene_to_packed(level_scene)
+		)
+		levels_container.add_child(b)
 
 
 func _on_quit_button_pressed() -> void:
